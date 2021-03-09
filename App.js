b545210774bc6fb2423dcb0813d0d1798ed523f1
Steven Mokoena <steventmokoena@gmail.com>
2021-03-09 11:38:14 +0200

    making an app navigation based - end of topic

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
 import React from "react";
 import EventList from "./EventList";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import EventForm from "./EventForm";
+
+const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
-  return <EventList />;
+  return (
+    <NavigationContainer>
+      <Navigator>
+        <Screen
+          name="list"
+          component={EventList}
+          options={{ title: "Your Events" }}
+        />
+        <Screen
+          name="form"
+          component={EventForm}
+          options={{ title: "Add an Event" }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
