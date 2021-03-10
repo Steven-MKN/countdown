c5d56f1491096e0eea520aea9343da16ffb1e28e
Steven Mokoena <steventmokoena@gmail.com>
2021-03-10 09:42:42 +0200

    using the textinput component - end of topic

--- EventForm.js
+++ EventForm.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
-import { View, Text, TouchableHighlight } from "react-native";
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  fieldContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: "#FFF",
+  },
+  text: {
+    height: 40,
+    margin: 0,
+    marginRight: 7,
+    paddingLeft: 10,
+  },
+});
 
 class EventForm extends Component {
   handleAddPress = () => {};
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
+        <view style={styles.fieldContainer}>
+          <TextInput
+            style={styles.text}
+            placeholder="Event title"
+            spellCheck={false}
+          />
+        </view>
         <TouchableHighlight onPress={this.handleAddPress}>
           <Text>Add</Text>
         </TouchableHighlight>
