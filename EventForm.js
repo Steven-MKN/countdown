a9605e93c22c043cbc71b930cfc016d3500eb4d8
Steven Mokoena <steventmokoena@gmail.com>
2021-03-18 15:35:04 +0200

    sending data to an http api - end of topic

--- EventForm.js
+++ EventForm.js
@@ -7,7 +7,7 @@ import {
   StyleSheet,
 } from "react-native";
 import DateTimePicker from "react-native-modal-datetime-picker";
-import { formatDateTime } from "./api";
+import { formatDateTime, saveEvent } from "./api";
 
 const styles = StyleSheet.create({
   fieldContainer: {
@@ -49,8 +49,7 @@ class EventForm extends Component {
   };
 
   handleAddPress = () => {
-    console.log(this.state);
-    this.props.navigation.goBack();
+    saveEvent({ ...this.state }).then(() => this.props.navigation.goBack());
   };
 
   handleChangeTitle = (value) => {
--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 import moment from "moment";
 import Constents from "expo-constants";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
 
 const { manifest } = Constents;
 const api = manifest.packagerOpts.dev
@@ -12,6 +14,22 @@ export function getEvents() {
   return fetch(url)
     .then((response) => response.json())
     .then((events) => events.map((e) => ({ ...e, date: new Date(e.date) })))
+    .catch((err) => console.log(err));
+}
+
+export function saveEvent({ title, date }) {
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      date,
+      id: uuidv4(),
+    }),
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+  })
+    .then((res) => res.json())
     .catch((err) => console.log(err));
 }
 
